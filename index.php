2f6a410d06e53696dd1ec3f72c732830752ff105
Silas Tristoni <silas.tristoni12@gmail.com>
2024-09-11 22:19:52 -0300

    Mais alterações

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ include 'includes/header.php';
 ?>
 
 <main>
-    <div>
+    <div class="telainicial">
         <h1>Bem vindo</h1>
         <p>Nossa empresa é uma página de um fã apaixanoda com o grêmio, fundada em 2024, nossa diretriz é viver diaramente amando o imortal tricolor</p>
+        <img src="assets/img/telainicial.jpg" alt="">
     </div>
 </main>
 
